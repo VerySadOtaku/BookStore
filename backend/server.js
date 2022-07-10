525270ac85c51c8ccd9359776defb6d73c7c2414
VerySadOtaku <german.sichenikov@gmail.com>
2022-07-10 22:58:14 +0300

    Goals controller and route setup

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,17 @@
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
 
-// require('dotenv').config();
+const app = express()
 
-// const app = express();
-// const port = process.env.port || 5000;
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
 
-// app.use(cors());
-// app.use(express.json());
+app.use('/api/books', require('./routes/bookRoutes'))
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+app.use(errorHandler)
 
-// const connection = mongo.connection;
-// connection.once('open', () => {
-//     console.log('MongoDB database connected succesfully');
-// })
-
-
-// app.listen(port, () => {
-//     console.log(`Server is running on port: ${port}`);
-// })
-
-console.log('hello, world!')
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
