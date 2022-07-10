70b4506493a7a19c87bc153ec7031c11f5766d77
VerySadOtaku <german.sichenikov@gmail.com>
2022-07-10 16:55:40 +0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+
+// require('dotenv').config();
+
+// const app = express();
+// const port = process.env.port || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+// const connection = mongo.connection;
+// connection.once('open', () => {
+//     console.log('MongoDB database connected succesfully');
+// })
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// })
+
+console.log('hello, world!')
